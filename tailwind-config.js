9d2cac86cef2b4b5674357be40932142d6bd06a5
thierroller55-source <thierroller55@gmail.com>
2026-02-01 22:07:53 +0000

    tailwind

---
+++ tailwind-config.js
@@ -0,0 +1,59 @@
+ 
+        tailwind.config = {
+            theme: {
+                //screens:{
+                    //tablet:"600px",
+               // },
+                extend: {
+                    colors: {
+                        vert: '#32a852',
+                        blanc: '#f7faf8',
+                        rouge: '#d10210',
+                        meuve: '#8902d1',
+                        blanc: '#faf7f8', 
+                        bleu: '#0b1bd4',
+                        black:'#0a0a0a',
+                        zone:'#d9d507',
+                        orange:'#d95707', 
+                        siro:"#0cd2e8", 
+                        gris:"#4a4f4b",
+                        gris_clair:"#ededed",
+                        vert_d:"#a4bdab",                  
+                    },
+                     backgroundImage: {
+                        profil: "url('images_tlwd/profil-0.png')",
+                    },
+                    boxShadow: {
+                        bs:"2px 10px 10px 8px rgba(15, 15, 15,0.5)",
+                    },
+                    borderWidth:{
+                        bw:"8px",
+                    },
+                    borderColor:{
+                        bc:"#d4830b",
+                        bl:"#344ceb",
+                        siro:"#0cd2e8",
+                    },
+                    borderRadius: {
+                        br:"20px",
+                        brfl:"9999px",
+                    },
+                      margin:{
+                        mg:"40px",
+                    },
+                      padding:{
+                        pdg:"40px",
+                    },
+                     height:{
+                        ht:"350px",
+                    },
+                      width:{
+                        wd:"40%",
+                    },
+                      gap:{
+                        gp:"16",
+                    },
+                }
+            }
+        }
+    
